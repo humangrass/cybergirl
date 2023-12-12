9650145988ca2263908fdf9a81531c6beadb6d83
Human Grass <legendary.grassman@gmail.com>
2023-12-12 22:50:56 +0300

    improve laser beam

--- characters/common/attack/laser/laser_beam.gd
+++ characters/common/attack/laser/laser_beam.gd
@@ -1,29 +1,47 @@
 extends RayCast2D
 
-@export var max_length = 4000
-@export var is_casting := false
+@export var max_length : float = 4000
+@export var animation_duration : float = 0.5
+@export var slowdown_factor : float = 4
 
-@onready var ray = $"."
-@onready var line = $Line2D
-@onready var end = $Marker2D
+@onready var ray : RayCast2D = $"."
+@onready var line : Line2D = $Line2D
+@onready var end : Marker2D = $Marker2D
+
+var animation_timer : float
+var current_length : float
+
 
 func _ready():
 	line.hide()
 	line.points = [10, 100]
 
-func _physics_process(delta):
-	print(delta)
-	var cast_point = get_local_mouse_position().normalized() * max_length
-	ray.target_position = cast_point
+func _process(delta: float) -> void:
+	handle_action()
+	if line.visible:
+		animation_timer += delta
+		var progress : float = animation_timer / animation_duration
+		if progress > 1:
+			progress = 1
 
+		var target : Vector2 = get_local_mouse_position().normalized() * current_length
+		var cast_point : Vector2 = ray.target_position.lerp(target, delta * slowdown_factor)
+		ray.target_position = cast_point
+
+		if is_colliding():
+			cast_point = to_local(get_collision_point())
+			current_length = cast_point.length()
+		else:
+			current_length = lerp(0.0, max_length, progress)
+		line.points[1] = cast_point
+
+
+func handle_action():
 	if Input.is_action_pressed("mouse_left"):
-		if line.visible != true:
+		if not line.visible:
+			animation_timer = 0.0
+			ray.target_position = get_local_mouse_position().normalized()
 			line.show()
 	else:
-		if line.visible == true:
+		if line.visible:
 			line.hide()
-
-	if is_colliding():
-		cast_point = to_local(get_collision_point())
-	line.points[1] = cast_point
-	
